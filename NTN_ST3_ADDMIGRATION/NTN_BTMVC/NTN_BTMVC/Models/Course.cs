ae4d9717861e549a7ce47f3cdd0e8e91806e4fcb
ThanhNguyen <nguyen5678212@gmail.com>
2026-04-21 20:58:00 +0700

    step3

---
+++ NTN_ST3_ADDMIGRATION/NTN_BTMVC/NTN_BTMVC/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace NTN_BTMVC.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public int CourseName { get; set; }
+        public int Credits {  get; set; }
+
+        public ICollection<ClassSection> ClassSections { get; set; }
+    }
+}
