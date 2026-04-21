ee08ffba0437b8980ced83bc05467a5856f02e42
Thanh Nguyen hs <ntn5678212@gmail.com>
2026-04-21 21:18:39 +0700

    Delete NTN_ST3_CREATECONTROLLOER/NTN_BTMVC directory
    xóa file git nhầm

--- NTN_ST3_CREATECONTROLLOER/NTN_BTMVC/NTN_BTMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NTN_BTMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
